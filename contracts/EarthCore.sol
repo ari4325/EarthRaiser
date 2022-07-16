3da221e561b2fdda34b120984f5d13fc1cfed444
ari4325 <ari4325@live.com>
2022-07-16 12:52:48 +0530

    initial commit

--- contracts/EarthCore.sol
+++ contracts/EarthCore.sol
@@ -3,28 +3,7 @@ pragma solidity 0.8.15;
 
 import "./EarthNFT.sol";
 
-interface IERC20 {
-
-    event Transfer(address indexed from, address indexed to, uint256 value);
-
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-
-    function totalSupply() external view returns (uint256);
-
-    function balanceOf(address account) external view returns (uint256);
-
-    function transfer(address to, uint256 amount) external returns (bool);
-
-    function allowance(address owner, address spender) external view returns (uint256);
-
-    function approve(address spender, uint256 amount) external returns (bool);
-
-    function transferFrom(
-        address from,
-        address to,
-        uint256 amount
-    ) external returns (bool);
-}
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract EarthCore {
 
